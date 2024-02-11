f02b6f586f4e96b4c49b1b0ed3238efeeb766268
KDF25 <karimov.damir.faridovich@gmail.com>
2024-02-11 16:30:12 +0500

    fix: change SVGR

--- src/shared/assets/icons/more.tsx
+++ src/shared/assets/icons/more.tsx
@@ -5,28 +5,28 @@ export const MoreIcon = () => (
   <rect x="18.5" y="0.5" width="5" height="5.25" rx="2.5" fill="url(#paint4_linear_720_33486)" stroke="url(#paint5_linear_720_33486)"/>
   <defs>
     <linearGradient id="paint0_linear_720_33486" x1="-0.123288" y1="3.125" x2="3.07884" y2="0.0665811" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#4772E6"/>
-      <stop offset="1" stop-color="#8E54E9"/>
+      <stop stopColor="#4772E6"/>
+      <stop offset="1" stopColor="#8E54E9"/>
     </linearGradient>
     <linearGradient id="paint1_linear_720_33486" x1="-0.123288" y1="3.125" x2="3.07884" y2="0.0665811" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#4772E6"/>
-      <stop offset="1" stop-color="#8E54E9"/>
+      <stop stopColor="#4772E6"/>
+      <stop offset="1" stopColor="#8E54E9"/>
     </linearGradient>
     <linearGradient id="paint2_linear_720_33486" x1="8.87671" y1="3.125" x2="12.0788" y2="0.0665811" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#4772E6"/>
-      <stop offset="1" stop-color="#8E54E9"/>
+      <stop stopColor="#4772E6"/>
+      <stop offset="1" stopColor="#8E54E9"/>
     </linearGradient>
     <linearGradient id="paint3_linear_720_33486" x1="8.87671" y1="3.125" x2="12.0788" y2="0.0665811" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#4772E6"/>
-      <stop offset="1" stop-color="#8E54E9"/>
+      <stop stopColor="#4772E6"/>
+      <stop offset="1" stopColor="#8E54E9"/>
     </linearGradient>
     <linearGradient id="paint4_linear_720_33486" x1="17.8767" y1="3.125" x2="21.0788" y2="0.0665811" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#4772E6"/>
-      <stop offset="1" stop-color="#8E54E9"/>
+      <stop stopColor="#4772E6"/>
+      <stop offset="1" stopColor="#8E54E9"/>
     </linearGradient>
     <linearGradient id="paint5_linear_720_33486" x1="17.8767" y1="3.125" x2="21.0788" y2="0.0665811" gradientUnits="userSpaceOnUse">
-      <stop stop-color="#4772E6"/>
-      <stop offset="1" stop-color="#8E54E9"/>
+      <stop stopColor="#4772E6"/>
+      <stop offset="1" stopColor="#8E54E9"/>
     </linearGradient>
   </defs>
 </svg>
