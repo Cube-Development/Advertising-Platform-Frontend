887bea81f606accb967d9159a2194ec5f149278c
AbsSh <macbro@Abdsh.local>
2024-02-12 19:27:11 +0500

    feat: fix auth logic with 401, save in cookies role from accessToken, authGenCode with links

--- src/widgets/header/UI/loginBtn/ui.tsx
+++ src/widgets/header/UI/loginBtn/ui.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import styles from "./styles.module.scss";
 import { MyButton } from "@shared/ui";
 import { useTranslation } from "react-i18next";
@@ -7,22 +7,23 @@ import { AuthStateGenerator } from "@features/authStateGenerator";
 
 export const LoginBtn: FC = () => {
   const { t } = useTranslation();
-  const [genCode, setGenCode] = useState<number>();
-  const handleAuth = () => {
-    const generetedCode = AuthStateGenerator();
-    setGenCode(generetedCode);
+
+  const handleRegistrationClick = () => {
+    const { registrationLink } = AuthStateGenerator();
+    window.location.href = registrationLink;
   };
 
-  const loginLink = `https://auth.cubinc.uz/realms/advertisement-blogger/protocol/openid-connect/auth?response_type=code&client_id=adv-blog-client&state=${genCode}&scope=openid profile&redirect_uri=https://ad.cubinc.uz
-  `;
-  const registrationLink = `https://auth.cubinc.uz/realms/advertisement-blogger/protocol/openid-connect/registrations?response_type=code&client_id=adv-blog-client&state=${genCode}&scope=openid profile&redirect_uri=https://ad.cubinc.uz`;
+  const handleLoginClick = () => {
+    const { loginLink } = AuthStateGenerator();
+    window.location.href = loginLink;
+  };
 
   return (
     <div className={styles.wrapper}>
-      <a onClick={handleAuth} href={registrationLink}>
+      <a onClick={handleRegistrationClick}>
         <MyButton>{t("registration")}</MyButton>
       </a>
-      <a onClick={handleAuth} href={loginLink}>
+      <a onClick={handleLoginClick}>
         <div className={styles.loginBtn}>
           <button>{t("login")}</button>
           <LoginIcon />
