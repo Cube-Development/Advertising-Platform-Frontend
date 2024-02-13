80510c6dec55dfa099cd143c55c98d5f57090162
AbsSh <macbro@Abdsh.local>
2024-02-13 17:25:26 +0500

    feat: check ci/cd

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,12 @@ import { Routing } from "@pages/index";
 import { withProviders } from "./providers";
 
 const App: React.FC = () => {
-  return <Routing />;
+  return (
+    <>
+      <Routing />
+      <h1>CI/CD PIPELINE YEEEEAAAAAAAHHHH</h1>
+    </>
+  );
 };
 
 export default withProviders(App);
