ffb8536bce5191219015eb20e41fc1e1bb69fb95
AbddSsh <abduraimovshakh@gmail.com>
2024-05-29 16:06:27 +0500

    feat: merge with adaptive

--- src/features/advProjectCard/UI/index.tsx
+++ src/features/advProjectCard/UI/index.tsx
@@ -81,7 +81,7 @@ export const AdvProjectCard: FC<AdvProjectCardProps> = ({
 
   const handleClickOutside = () => {
     const state = (accordionRef.current! as HTMLElement).getAttribute(
-      "data-state"
+      "data-state",
     );
     state === accordionTypes.open
       ? setSubcardOpen(true)
