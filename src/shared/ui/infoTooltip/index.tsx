8c75bed32a3f946d58172a73ce237d45f428ab07
KDF25 <karimov.damir.faridovich@gmail.com>
2025-02-01 23:56:59 +0500

    fix: add app Tooltip main provider and add onClick open in infoTooltip & styles

--- src/shared/ui/infoTooltip/index.tsx
+++ src/shared/ui/infoTooltip/index.tsx
@@ -1,14 +1,9 @@
-import { FC } from "react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../shadcn-ui";
-import styles from "./styles.module.scss";
-import { useWindowWidth } from "@shared/hooks";
 import { BREAKPOINT } from "@shared/config";
+import { useWindowWidth } from "@shared/hooks";
 import { InfoIcon } from "lucide-react";
+import { FC, useState } from "react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../shadcn-ui";
+import styles from "./styles.module.scss";
 
 export interface InfoTooltipProps {
   text: string;
@@ -16,19 +11,24 @@ export interface InfoTooltipProps {
 
 export const InfoTooltip: FC<InfoTooltipProps> = ({ text }) => {
   const screen = useWindowWidth();
+  const [open, setOpen] = useState(false);
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger type="button" className={styles.trigger}>
-          <InfoIcon color="#BDBDBD" />
-        </TooltipTrigger>
-        <TooltipContent
-          side={`${screen > BREAKPOINT.MD ? "right" : "top"}`}
-          className={styles.content}
-        >
-          <p className={styles.text}>{text}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Tooltip open={open}>
+      <TooltipTrigger
+        type="button"
+        className={styles.trigger}
+        onClick={() => setOpen(!open)}
+        onMouseEnter={() => setOpen(true)}
+        onMouseLeave={() => setOpen(false)}
+      >
+        <InfoIcon color="#BDBDBD" />
+      </TooltipTrigger>
+      <TooltipContent
+        side={`${screen > BREAKPOINT.MD ? "right" : "top"}`}
+        className={styles.content}
+      >
+        <p className={styles.text}>{text}</p>
+      </TooltipContent>
+    </Tooltip>
   );
 };
