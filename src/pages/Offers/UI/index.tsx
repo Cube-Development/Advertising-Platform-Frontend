2f3915da9c0dd369ae6f34bf1b86df8aeb405918
KDF25 <karimov.damir.faridovich@gmail.com>
2025-03-01 15:39:51 +0500

    fix: blogeger offers

--- src/pages/Offers/UI/index.tsx
+++ src/pages/Offers/UI/index.tsx
@@ -1,93 +1,9 @@
-// import {
-//   getOrdersByStatusReq,
-//   offerStatusFilter,
-//   useGetBloggerOrdersQuery,
-// } from "@entities/offer";
-// import {
-//   dateSortingTypes,
-//   platformTypes,
-//   platformTypesNum,
-// } from "@entities/platform";
-// import { useGetViewBloggerOrderQuery } from "@entities/views";
-// import { INTERSECTION_ELEMENTS, Languages } from "@shared/config";
-// import { pageFilter } from "@shared/routing";
-// import { BarFilter } from "@widgets/barFilter";
-// import { MyOffers } from "@widgets/offer";
-// import { FC, useEffect, useState } from "react";
-// import { useForm } from "react-hook-form";
-// import styles from "./styles.module.scss";
-
-// export const OffersPage: FC = () => {
-//   const page = pageFilter.offer;
-// const language = useFindLanguage();
-
-//   const { setValue, watch } = useForm<{
-//     platform: platformTypesNum;
-//     status: offerStatusFilter | string;
-//   }>({
-//     defaultValues: {
-//       platform: platformTypes[0].id,
-//       status: offerStatusFilter.active,
-//     },
-//   });
-//   const formState = watch();
-
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const handleOnChangePage = () => {
-//     setCurrentPage(currentPage + 1);
-//   };
-
-//   const getParams: getOrdersByStatusReq = {
-//     language: language?.id || Languages[0].id,
-//     page: currentPage,
-//     elements_on_page: INTERSECTION_ELEMENTS.bloggerOffers,
-//     date_sort: dateSortingTypes.decrease,
-//     status: formState.status,
-//   };
-
-//   const { data, isFetching } = useGetBloggerOrdersQuery(getParams);
-//   const { refetch: views } = useGetViewBloggerOrderQuery();
-
-//   useEffect(() => {
-//     setCurrentPage(1);
-//   }, [formState.status]);
-
-//   useEffect(() => {
-//     views();
-//   }, [currentPage, formState.status]);
-
-//   return (
-//     <div className="container">
-//       <div className={styles.wrapper}>
-//         <BarFilter
-//           page={page}
-//           listLength={!!data?.orders?.length}
-//           setValue={setValue}
-//           changeStatus={(status) => setValue("status", status)}
-//           statusFilter={formState.status}
-//         />
-//         <MyOffers
-//           statusFilter={formState.status as offerStatusFilter}
-//           offers={(data?.status === formState.status && data?.orders) || []}
-//           handleOnChangePage={handleOnChangePage}
-//           isLoading={isFetching}
-//           isLast={data?.isLast || false}
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
 import {
   getOrdersByStatusReq,
   offerStatusFilter,
   useGetBloggerOrdersQuery,
 } from "@entities/offer";
-import {
-  dateSortingTypes,
-  platformTypes,
-  platformTypesNum,
-} from "@entities/platform";
+import { dateSortingTypes } from "@entities/platform";
 import { useFindLanguage } from "@entities/user";
 import { useGetViewBloggerOrderQuery } from "@entities/views";
 import { INTERSECTION_ELEMENTS, Languages } from "@shared/config";
@@ -109,15 +25,14 @@ export const OffersPage: FC = () => {
   const page = pageFilter.offer;
   const language = useFindLanguage();
 
-  const { setValue, watch } = useForm<{
-    platform: platformTypesNum;
-    status: offerStatusFilter | string;
-    page: number;
-  }>({
+  const { setValue, watch } = useForm<getOrdersByStatusReq>({
     defaultValues: {
-      platform: platformTypes[0].id,
+      // platform: platformTypes[0].id,
       status: offerStatusFilter.active,
       page: 1,
+      language: language?.id || Languages[0].id,
+      elements_on_page: INTERSECTION_ELEMENTS.bloggerOffers,
+      date_sort: dateSortingTypes.decrease,
     },
   });
   const formState = watch();
@@ -127,27 +42,12 @@ export const OffersPage: FC = () => {
     setValue("status", status);
   };
 
-  // const [currentPage, setCurrentPage] = useState(1);
-
-  const getParams: getOrdersByStatusReq = {
-    language: language?.id || Languages[0].id,
-    page: formState.page,
-    elements_on_page: INTERSECTION_ELEMENTS.bloggerOffers,
-    date_sort: dateSortingTypes.decrease,
-    status: formState.status,
-  };
-
-  const { data, isFetching } = useGetBloggerOrdersQuery(getParams);
+  const { data, isFetching } = useGetBloggerOrdersQuery(formState);
   const { refetch: views } = useGetViewBloggerOrderQuery();
 
   const handleOnChangePage = () => {
-    // setCurrentPage((prev) => prev + 1);
     setValue("page", formState.page + 1);
   };
-
-  // useEffect(() => {
-  //   setCurrentPage(1);
-  // }, [formState.status]);
 
   useEffect(() => {
     views();
@@ -161,7 +61,6 @@ export const OffersPage: FC = () => {
             page={page}
             listLength={!!data?.orders?.length}
             setValue={setValue}
-            // changeStatus={(status) => setValue("status", status)}
             changeStatus={changeStatus}
             statusFilter={formState.status}
           />
