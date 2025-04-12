8a3b28c279cecbab4f401d5ee748ee6945558121
KDF25 <karimov.damir.faridovich@gmail.com>
2025-04-12 22:57:04 +0500

    feat: add not found page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.f1l3l2qulg8"
+    "revision": "0.7qgk87vtjio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
