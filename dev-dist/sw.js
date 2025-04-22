b72dd717c2d014fe56d10ae3cb010deeb1ebd6b5
AbddSsh <abduraimovshakh@gmail.com>
2025-04-22 10:31:47 +0700

    feat: mainPage steps animation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.i1a2bdvd5kg"
+    "revision": "0.urivt7sseag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
