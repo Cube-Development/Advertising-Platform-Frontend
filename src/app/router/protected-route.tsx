77b6671c601df1304d0f0366ba759a3ef7587052
KDF25 <karimov.damir.faridovich@gmail.com>
2025-05-25 00:31:23 +0500

    feat: add notifications access page logic

--- src/app/router/protected-route.tsx
+++ src/app/router/protected-route.tsx
@@ -1,12 +1,16 @@
-import { ENUM_ROLES } from "@entities/user";
-import { useAppSelector } from "@shared/hooks";
+import { ENUM_ROLES, toggleRole } from "@entities/user";
+import { useAppDispatch, useAppSelector } from "@shared/hooks";
 import { ENUM_AUTH_TYPES, ENUM_PATHS, IRouting } from "@shared/routing";
 import { SideBarAdminLayout, SideBarLayout } from "@widgets/layouts";
-import { Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Navigate, useLocation } from "react-router-dom";
 
 // Компонент защиты маршрутов
 export const ProtectedRoute = ({ route }: { route: IRouting }) => {
-  const { isAuth, role } = useAppSelector((state) => state.user);
+  const { isAuth, role: sliceRole } = useAppSelector((state) => state.user);
+  let role: ENUM_ROLES = sliceRole;
+  const location = useLocation();
+  const dispatch = useAppDispatch();
 
   // Проверка на авторизацию
   if (route.auth === ENUM_AUTH_TYPES.ONLY_PUBLIC && isAuth) {
@@ -16,6 +20,21 @@ export const ProtectedRoute = ({ route }: { route: IRouting }) => {
     return <Navigate to={ENUM_PATHS.MAIN} replace />;
   }
 
+  const query = new URLSearchParams(location.search);
+  const isAccess = query.get("access") === "true";
+
+  // Если есть доступ, то устанавливаем роль в куки
+  useEffect(() => {
+    if (isAccess) {
+      dispatch(toggleRole(route?.roles?.[0] as ENUM_ROLES));
+    }
+  }, [isAccess]);
+
+  if (isAccess) {
+    role = route?.roles?.[0] as ENUM_ROLES;
+  }
+
+  // Проверка на доступность публичного маршрута в зависимости от роли
   if (route.auth === ENUM_AUTH_TYPES.PUBLIC) {
     if (role === ENUM_ROLES.BLOGGER && !route.roles?.includes(role)) {
       return <Navigate to={ENUM_PATHS.MAIN_BLOGGER} replace />;
@@ -27,8 +46,7 @@ export const ProtectedRoute = ({ route }: { route: IRouting }) => {
       return <Navigate to={ENUM_PATHS.ORDERS} replace />;
     }
   }
-
-  // Проверка на роль
+  // Проверка на доступность остальных маршрутов в зависимости от роли
   if (route.roles && !route.roles.includes(role)) {
     if (role === ENUM_ROLES.MODERATOR) {
       return <Navigate to={ENUM_PATHS.ADMIN_HOME} replace />;
@@ -36,6 +54,7 @@ export const ProtectedRoute = ({ route }: { route: IRouting }) => {
     if (role === ENUM_ROLES.MANAGER) {
       return <Navigate to={ENUM_PATHS.ORDERS} replace />;
     }
+    console.log("Access denied for role", role);
     return <Navigate to={ENUM_PATHS.MAIN} replace />;
   }
 
