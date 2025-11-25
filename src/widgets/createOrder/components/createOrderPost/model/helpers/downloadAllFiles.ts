7490d5409c6a4488fa644d2ebc33d5bb162d0a2c
AbddSsh <abduraimovshakh@gmail.com>
2025-11-25 10:41:26 +0500

    feat: url content in posts request

--- src/widgets/createOrder/components/createOrderPost/model/helpers/downloadAllFiles.ts
+++ src/widgets/createOrder/components/createOrderPost/model/helpers/downloadAllFiles.ts
@@ -2,8 +2,9 @@ import { IPostData } from "@entities/project";
 import { downloadFile } from "@shared/utils";
 
 export const downloadAllFiles = async (files: IPostData[]) => {
-  const promises: Promise<File>[] = files.map((file) =>
-    downloadFile(file.url || "", file.content),
+  const promises: Promise<File>[] = files.map(
+    (file) => downloadFile(file.content || "", file.name || ""),
+    // downloadFile(file.url || "", file.name || ""),
   );
   return await Promise.all(promises);
 };
