03cfd29a58e84dfdeab35b6fb2dc6de205c8009b
KDF25 <karimov.damir.faridovich@gmail.com>
2026-01-30 12:43:52 +0300

    fix: fix merge

--- src/widgets/voice-agent/model/constants/constants.ts
+++ src/widgets/voice-agent/model/constants/constants.ts
@@ -1,5 +1,5 @@
-// export const LIVEKIT_TOKEN_ENDPOINT = "http://3.124.203.102:5001/getToken";
-export const LIVEKIT_TOKEN_ENDPOINT = "http://127.0.0.1:5001/getToken";
+export const LIVEKIT_TOKEN_ENDPOINT = "http://3.124.203.102:5001/getToken";
+// export const LIVEKIT_TOKEN_ENDPOINT = "http://127.0.0.1:5001/getToken";
 
 export const STORAGE_KEYS = {
   MICROPHONE_DEVICE: "livekit-microphone-device",
